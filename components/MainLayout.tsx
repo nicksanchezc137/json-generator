ad1bce24d992f196160b8146aab856a5be67e05d
Nick Korage <nick@pinholepro.com>
2023-12-20 08:02:49 +0300

    main layout bg

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -6,7 +6,7 @@ import React, { ReactHTML } from "react";
     children: JSX.Element | JSX.Element[];
   }) {
     return (
-      <div className="w-full flex items-center justify-center">
+      <div className="w-full flex items-center justify-center bg-white">
         <div className="xl:max-w-[75rem] md:w-[75%] w-full flex items-start mt-[4rem] mb-10">
          
           {children}
